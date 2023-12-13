72107c72bbdf51c36a4deb3c654e0a4a5ce1c298
dacbac2702 <bac27022002@gmail.com>
2023-12-14 04:15:43 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -15,28 +15,52 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(title: Text('App Title')),
         body: Center(
-          // body: const Center(
-
-          child: Row(
-            // child: Text(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: Column(
             children: [
-              Text(
-                'I', // 'Hello, World!',
-                style: Theme.of(context).textTheme.headline2,
+              Container(
+                height: 300,
+                width: double.infinity,
+                color: Colors.deepOrangeAccent,
               ),
-              Icon(
-                Icons.favorite,
-                color: Colors.redAccent,
-                size: 44,
+              CircleAvatar(
+                radius: 60,
+                child: Icon(Icons.face, size: 120),
               ),
-              Text(
-                'Flutter',
-                style: Theme.of(context).textTheme.headline2,
+              Container(
+                padding: EdgeInsets.only(left: 10, right: 10),
+                height: 140,
+                width: double.infinity,
+                child: Card(
+                  color: Colors.amber,
+                  elevation: 5,
+                ),
               ),
             ],
-          ), // ),
-        ), // ),
+          ),
+        ),
+        // body: Center(
+        //   // body: const Center(
+
+        //   child: Row(
+        //     // child: Text(
+        //     mainAxisAlignment: MainAxisAlignment.center,
+        //     children: [
+        //       Text(
+        //         'I', // 'Hello, World!',
+        //         style: Theme.of(context).textTheme.headline2,
+        //       ),
+        //       Icon(
+        //         Icons.favorite,
+        //         color: Colors.redAccent,
+        //         size: 44,
+        //       ),
+        //       Text(
+        //         'Flutter',
+        //         style: Theme.of(context).textTheme.headline2,
+        //       ),
+        //     ],
+        //   ), // ),
+        // ), // ),
       ),
     );
   }
